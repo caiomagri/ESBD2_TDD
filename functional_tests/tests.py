6338e0b5610d704eb2a804ebebd83e3487c4f646
Caio Magri <kaioeduardofm@gmail.com>
2022-05-23 23:16:26 -0300

    Adicionando teste para criar item na lista

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,6 +14,20 @@ class NewVsitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	# Auxiliary method 
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+				return
+			except(AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+
 	def test_can_start_a_list_for_one_user(self):
 		# Edith ouviu falar que agora a aplicação online de lista de tarefas
 		# aceita definir prioridades nas tarefas do tipo baixa, média e alta
@@ -39,3 +53,8 @@ class NewVsitorTest(LiveServerTestCase):
 		# Ela digita "Comprar anzol" em uma nova caixa de texto
 		# e assinala prioridade alta no campo de seleção de prioridades
 		inputbox.send_keys('Comprar anzol')
+		# Quando ela tecla enter, a página é atualizada, e agora
+		# a página lista "1 - Comprar anzol - prioridade alta"
+		# como um item em uma lista de tarefas
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1 - Comprar anzol - prioridade alta')
